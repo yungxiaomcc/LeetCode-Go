df6baba3fa34f201213dbbb63824aeb19ba4b8a4
xiaoyang.yx <xiaoyang.yx@xuelangyun.com>
2022-04-15 18:06:38 +0800

    update

---
+++ leetcode/0001.Two-Sum/py_demo.py
@@ -0,0 +1,18 @@
+# python 实现
+
+
+def main(nums:list,target:int)->list:
+    m = list()
+    for index,item in enumerate(nums):
+        import pdb
+
+
+    pass
+
+
+
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    main(nums,target)
